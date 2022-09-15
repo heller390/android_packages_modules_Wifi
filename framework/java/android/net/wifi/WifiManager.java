6403d16db4aec016e94f4d842e59627665fe8d14
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-09-15 13:15:45 +0300

    gmscompat: remove stubs that are now handled by GmsCompatConfig
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- framework/java/android/net/wifi/WifiManager.java
+++ framework/java/android/net/wifi/WifiManager.java
@@ -1766,9 +1766,6 @@ public class WifiManager {
     @NonNull
     public List<Pair<WifiConfiguration, Map<Integer, List<ScanResult>>>> getAllMatchingWifiConfigs(
             @NonNull List<ScanResult> scanResults) {
-        if (GmsCompat.isEnabled()) {
-            return Collections.emptyList();
-        }
         List<Pair<WifiConfiguration, Map<Integer, List<ScanResult>>>> configs = new ArrayList<>();
         try {
             Map<String, Map<Integer, List<ScanResult>>> results =
